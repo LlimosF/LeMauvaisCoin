859b7971df1b8cf7694d8c808de6f3f41135e55e
LlimosF <llimosflorian1>
2023-08-03 13:40:16 +0200

    Commit

--- Controller/userClass.php
+++ Controller/userClass.php
@@ -2,8 +2,26 @@
 
 class User {
 
-}
+  private $lastname;
+  private $firstname;
+  private $email;
+  private $number;
+  private $password;
+  private $country;
+  private $city;
+  private $profilepicture;
 
-class DatabaseClass extends User {
+  public function __construct($lastname, $firstname, $email, $number, $password, $country, $city, $profilepicture) {
+
+    $this->lastname = $lastname;
+    $this->firstname = $firstname;
+    $this->email = $email;
+    $this->number = $number;
+    $this->password = $password;
+    $this->country = $country;
+    $this->city = $city;
+    $this->profilepicture = $profilepicture;
+
+  }
 
 }
